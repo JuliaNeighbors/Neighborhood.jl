6c9de8a370989347b2dde9baed8a55e940bd67dc
George Datseris <datseris.george@gmail.com>
2019-12-17 20:18:10 +0100

    remove search structure type

--- src/api.jl
+++ src/api.jl
@@ -2,11 +2,13 @@
 This file defines the common API for finding neighbors in Julia. For a package
 to participate in this common API it should extend the following methods:
 
-Let `SS` be your search structure. Make sure that this `SS` is a subtype of
+Let `T` be the type of your search structure. Make sure that this `SS` is a subtype of
 `SearchStrucutre`. Then define:
 
-* searchstructure(::T, data, metric)  (this gives `ss`, an instance of `SS`)
-* search(ss::SS, query, ::SearchType) (preferably for both SearchType types)
+* searchstructure(::Type{T}, data, metric)  (this gives `ss`)
+* search(ss::T, query, ::SearchType) (preferably for both SearchType types)
+
+Notice that ::Type{T} only needs the supertype, e.g. `KDTree`, without the type-parameters.
 =#
 
 export FixedRange, FixedAmount, SearchType
@@ -18,10 +20,18 @@ Supertype of all possible search types of the Neighborhood.jl common API.
 """
 abstract type SearchType end
 
+
 """
-Supertype of all structures that participate in the Neighborhood.jl common API.
+    searchstructure(T, data, metric; kwargs...) → st
+Create a search structure `st` of type `T` (e.g. `KDTree, BKTree` etc.) based on the
+given `data` and `metric`. The data types and supported metric types are package-specific,
+but typically all subtypes of `<:Metric` from Distances.jl are supported.
 """
-abstract type SearchStructure end
+function searchstructure(::Type{T}, data::D, metric::M; kwargs...) where
+                         {T, D, M}
+    error("Given type $(T) has not implemented the Neighborhood.jl public API "*
+          "for data type $(D) and metric type $(M).")
+end
 
 """
     FixedRange(r) <: SearchType
@@ -36,14 +46,14 @@ Search type representing the `k` nearest neighbors of the query.
 struct FixedAmount <: SearchType; k::Int; end
 
 """
-    search(ss::SearchStructure, query, st::SearchType; kwargs... ) → neighbors
+    search(ss, query, st::SearchType; kwargs... ) → neighbors
 Perform a neighbor search in the search structure `ss` for the given
 `query` with search type `st`.
 
 Package-specific keywords could be possible, see the specific nearest neighbor package
 for details.
 """
-function search(ss::SearchStructure, query, st::SearchType; kwargs...)
+function search(ss, query, st::SearchType; kwargs...)
     error("`search` is not implemented for this combination of input types.")
 end
 
@@ -62,41 +72,31 @@ in the search structure `ss`.
 knn(a, b, k::Integer; kwargs...) = search(a, b, FixedAmount(k); kwargs...)
 
 
-"""
-    searchstructure(T, data, metric; kwargs...) → st
-Create a search structure `st` of type `T` (e.g. `KDTree, BKTree` etc.) based on the
-given `data` and `metric`. The data types and supported metric types are package-specific,
-but typically all subtypes of `<:Metric` from Distances.jl are supported.
-"""
-function searchstructure(::Type{T}, data, metric; kwargs...) where {T<:SearchStructure}
-    error("Given type $(T) has not implemented the Neighborhood.jl public API.")
-end
-
 
 ##########################################################################################
 # Delete and insertion: mutable search structures
 ##########################################################################################
 
 """
-    insert!(ss::SearchStructure, index, point)
+    insert!(ss, index, point)
 Insert a new `point` to the search structure `ss` at the given `index`
 (similarly with `Base.insert!`).
 
 The `index` type is package-specific.
 """
-function Base.insert!(ss::T, index, point) where {T <: SearchStructure}
+function Base.insert!(ss::T, index, point) where {T}
     error("Given type $(T) has either not implemented the Neighborhood.jl public API "*
           "or uses an immutable search structure.")
 end
 
 """
-    deleteat!(ss::SearchStructure, index)
+    deleteat!(ss, index)
 Remove the point of the search structure specified by the given `index`
 (similarly with `Base.deletat!`).
 
 The `index` type is package-specific.
 """
-function Base.deleteat!(ss::T, index) where {T <: SearchStructure}
+function Base.deleteat!(ss::T, index) where {T}
     error("Given type $(T) has either not implemented the Neighborhood.jl public API "*
           "or uses an immutable search structure.")
 end
