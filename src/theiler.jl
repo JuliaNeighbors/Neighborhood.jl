c3e7a41b247edb5da1e0d415c24667a34edf3615
Datseris <datseris.george@gmail.com>
2020-11-30 18:16:25 +0100

    improve docstring of Theiler

--- src/theiler.jl
+++ src/theiler.jl
@@ -5,6 +5,7 @@ This is useful when the query of a search is also part of the data used to creat
 search structure, typical in timeseries analysis. In this case, you do not want to
 find the query itself as its neighbor, and you typically want to avoid points with
 indices very close to the query as well.
+Giving `w=0` excludes the query itself as its neighbor, see the boolean expressions below.
 
 Let `theiler = Theiler(w)`. Then, `theiler` by itself can be used as a skip function
 in [`bulksearch`](@ref), because `theiler(i, j) ≡ abs(i-j) ≤ w`.
@@ -14,7 +15,8 @@ queries in the original data)
 
 However `theiler` can also be used in single searches. `theiler(n)` (with one argument)
 generates the function `i -> abs(i-n) ≤ w`. So `theiler(n)` can be given to
-[`search`](@ref) as the `skip` argument.
+[`search`](@ref) as the `skip` argument. Notice that `theiler` is an instance of `Theiler`,
+and in summary you'd have to do `Theiler(w)(n)` and give that to [`search`](@ref).
 """
 struct Theiler{R} <: Function
     w::Int
